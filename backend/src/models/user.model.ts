ab811551d2e5eb6a36768eb9b0b5b27433a8a286
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-05 16:48:00 +0530

    Update user.model.ts
    Enable timestamps

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -6,6 +6,6 @@ const userSchema = new mongoose.Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true }
-})
+}, { timestamps: true })
 
 export const User = MongooseModel<IUser>('User', userSchema);
