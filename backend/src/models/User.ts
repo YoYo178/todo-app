2e9531b51c5067964ceb4ff7b9df093b80be14b9
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-05 14:15:38 +0530

    Update User model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -3,6 +3,7 @@ import { IUser } from '../types/user.types'
 import { MongooseModel } from '@src/utils/db.utils';
 
 const userSchema = new mongoose.Schema<IUser>({
+    name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true }
 })
