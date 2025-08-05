c10ea29318f6ab3337ba5a12b14dbdc82dccdafc
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-05 18:24:08 +0530

    Create pages, setup routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,41 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+import { AuthProvider } from "./contexts/AuthContext";
+import { TaskProvider } from "./contexts/TasksContext";
+
+import { PublicRouteLayout } from "./layouts/PublicRouteLayout";
+import { ProtectedRouteLayout } from "./layouts/ProtectedRouteLayout";
+
+import { Dashboard, Landing, Login, Register } from './pages'
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <h1>TODO!</h1>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <TaskProvider>
+          <BrowserRouter basename="/todo-app/">
+            <Routes>
+
+              <Route element={<PublicRouteLayout />}>
+                <Route index element={<Landing />} />
+
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+              </Route>
+
+              <Route element={<ProtectedRouteLayout />}>
+                <Route path='/dashboard' element={<Dashboard />} />
+              </Route>
+
+              <Route path="*" element={<h1>Not found!</h1>} />
+            </Routes>
+          </BrowserRouter>
+        </TaskProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,83 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Link, useLocation } from "react-router-dom";
+import { useSignupMutation } from "@/hooks/network/auth/useSignupMutation";
+import { useEffect } from "react";
+
+const signupSchema = z.object({
+    name: z.string().nonempty(),
+    email: z.email(),
+    password: z.string().min(8)
+})
+
+type RegisterForm = z.infer<typeof signupSchema>;
+
+export const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<RegisterForm>({
+        resolver: zodResolver(signupSchema),
+    });
+
+    const signupMutation = useSignupMutation({});
+    const location = useLocation();
+
+    const onSubmit = async (data: RegisterForm) => {
+        await signupMutation.mutateAsync({ payload: data });
+    };
+
+    useEffect(() => {
+        if (signupMutation.isIdle)
+            return;
+
+        if (!signupMutation.isSuccess)
+            alert("Error registering user");
+        else
+            alert("Register success!");
+    }, [signupMutation.isSuccess])
+
+    return (
+        <div className="h-screen w-screen flex items-center justify-center">
+            <Card className="w-[360px]">
+                <CardContent className="py-8">
+                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                        <div className="space-y-1">
+                            <Label htmlFor="name">Name</Label>
+                            <Input id="name" type="text" {...register("name")} />
+                            {errors.name && (
+                                <p className="text-sm text-red-500">{errors.name.message}</p>
+                            )}
+                        </div>
+                        <div className="space-y-1">
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" type="email" {...register("email")} />
+                            {errors.email && (
+                                <p className="text-sm text-red-500">{errors.email.message}</p>
+                            )}
+                        </div>
+                        <div className="space-y-1">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" type="password" {...register("password")} />
+                            {errors.password && (
+                                <p className="text-sm text-red-500">{errors.password.message}</p>
+                            )}
+                        </div>
+                        <Button type="submit" className="w-full" disabled={isSubmitting}>
+                            {isSubmitting ? "Registering..." : "Register"}
+                        </Button>
+                    </form>
+                    <p className="text-center text-sm mt-4">
+                        Already have an account? <Link to="/login" className="text-blue-600" state={{ from: location.pathname }} replace>Login</Link>
+                    </p>
+                </CardContent>
+            </Card>
+        </div>
+    );
+};
