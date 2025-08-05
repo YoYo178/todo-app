e042f01f597d09cacf87171f13822f7483487679
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2025-08-05 18:19:56 +0530

    Update task.schema.ts

--- backend/src/schemas/task.schema.ts
+++ backend/src/schemas/task.schema.ts
@@ -17,11 +17,11 @@ export const createTaskSchema = z.object({
 
 export type TCreateTaskBody = z.infer<typeof createTaskSchema>
 
-// update task body
+// Update task body
 export const updateTaskSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    rating: z.number().min(0).max(5)
+    title: z.string().optional(),
+    description: z.string().optional(),
+    rating: z.number().min(0).max(5).optional()
 })
 
 export type TUpdateTaskBody = Partial<z.infer<typeof updateTaskSchema>>
