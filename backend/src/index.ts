12f2f4b7d88fd65a6666e770293f8ae7f638c053
YoYo178 <95132723+YoYo178@users.noreply.github.com>
2026-07-29 19:50:44 +0530

    chore(backend): merge server.ts to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,64 @@
-import ENV from '@src/common/env.js';
-import server from './server.js';
+import express, { type NextFunction, type Request, type Response } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import ENV, { NODE_ENVS } from '@src/common/env.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import HTTP_STATUS_CODES from './common/HttpStatusCodes.js';
+import { CORSConfig } from './config/cors.config.js';
+import APIRouter from './routes/index.js';
 import { connectDB } from './config/db.config.js';
 
 /******************************************************************************
-                                Constants
+                                Setup
 ******************************************************************************/
 
-const SERVER_START_MSG = 'Express server started on port: ' + ENV.PORT.toString();
+await connectDB();
 
-/******************************************************************************
-                                  Run
-******************************************************************************/
+const app = express();
 
-connectDB();
+// **** Middleware **** //
 
-// Start the server
-server.listen(ENV.PORT, (err) => {
+app.use(cors(CORSConfig));
+app.use(cookieParser());
+
+// Basic middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Show routes called in console during development
+if (ENV.NODE_ENV === NODE_ENVS.DEVELOPMENT) {
+  app.use(morgan('dev'));
+}
+
+// Security
+if (ENV.NODE_ENV === NODE_ENVS.PRODUCTION) {
+  // eslint-disable-next-line n/no-process-env
+  if (!ENV.DISABLE_HELMET) {
+    app.use(helmet());
+  }
+}
+
+app.use('/api', APIRouter);
+
+// Add error handler
+app.use((err: Error, _: Request, res: Response, _next: NextFunction) => {
+  if (ENV.NODE_ENV !== NODE_ENVS.TEST.valueOf()) {
+    console.error(err, true);
+  }
+
+  res
+    .status(HTTP_STATUS_CODES.InternalServerError)
+    .json({ message: err?.message ?? 'Something went wrong' });
+});
+
+const PORT = ENV.PORT;
+app.listen(PORT, (err) => {
   if (!!err) {
     console.error(err.message);
-  } else {
-    console.info(SERVER_START_MSG);
+    return;
   }
+  
+  console.info(`Express server started on port ${PORT}`);
 });
